1d7067d04542634014eba2fa9bd61be15c3b4df8
luckyBambooBro <huypham008@hotmail.com>
2026-02-13 15:59:15 +1030

    corrected commandMap() accidentally added LocationArea struct, but keep it as it will come in handy for later

--- command_map.go
+++ command_map.go
@@ -1,35 +1,50 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 )
 
 const pokeLocationAreaURL = "https://pokeapi.co/api/v2/location-area/"
-var mapPage int
 
 func commandMap() error {
-	mapPage++
 	//get http.Response
 	resp, err := http.Get(pokeLocationAreaURL)
 	if err != nil {
 		return fmt.Errorf("error obtaining http.get response from %s: %w", pokeLocationAreaURL, err)
 	}
 	//convert resp to io.Reader type so we can Unmarshal
-	body, err := io.ReadAll(resp.Body)
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return fmt.Errorf("unable to obtain body of http.response: %w", err)
 	}
 	if resp.StatusCode > 299 {
 		return fmt.Errorf("bad status code: %d", resp.StatusCode)
 	}
+	//can replace this line later without defer if wanted
 	defer resp.Body.Close()
-
-	return nil
+	
+	var APIResourceListData APIResourceList
+	if err = json.Unmarshal(data, &APIResourceListData); err != nil {
+		return fmt.Errorf("Error unmarshaling http response body: %w", err)
 	}
 
+	fmt.Printf("%v\n", APIResourceListData)
+	//increment map page for next function call to call the next page of 20 results
+	return nil
 }
+
+
+//struct to capture APIResourceList via http.Get()
+type APIResourceList struct {
+	Count int `json:"count"`
+	Next string `json:"next"`
+	Previous string `json:"previous"`
+	Results []NameAndUrl `json:"results"`
+}
+
 //struct to capture json data. fields may have nested structs to capture structured data, there is one anonymous struct 
 //at the bottom due to single use and to keep it clean
 type LocationArea struct {

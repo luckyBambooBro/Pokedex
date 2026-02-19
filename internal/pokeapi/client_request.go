840c7dad627c94ad1773214461bb28ab2fed7c51
luckyBambooBro <huypham008@hotmail.com>
2026-02-19 21:47:23 +1030

    up to Ch2L2 still but step 6 now

--- internal/pokeapi/client_request.go
+++ internal/pokeapi/client_request.go
@@ -16,28 +16,41 @@ func (c *Client) ClientRequest(url string) (ApiStruct, error) {
 	if url == "" {
 		url = baseURL
 	}
-
-	//create request and send with client.Do()
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return ApiStruct{}, fmt.Errorf("unable to create http request: %w", err)
-	}
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return ApiStruct{}, fmt.Errorf("http.Client.Do error, unable to create http request: %w", err)
-	}
 	
-	defer resp.Body.Close()
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return ApiStruct{}, fmt.Errorf("unable to read http response body: %w", err)
+	//check if url is cached
+	data, ok := c.cache.Get(url)
+	//if not cached:
+	if !ok {
+		//create request and send with client.Do()
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return ApiStruct{}, fmt.Errorf("unable to create http request: %w", err)
+		}
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return ApiStruct{}, fmt.Errorf("http.Client.Do error, unable to create http request: %w", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode > 299 {
+			return ApiStruct{}, fmt.Errorf("response failed with status code: %d", resp.StatusCode)
+		}
+		
+		data, err = io.ReadAll(resp.Body) //does data on this line equal data from the earlier line  that checked the cache.Get() method
+		if err != nil {
+			return ApiStruct{}, fmt.Errorf("unable to read http response body: %w", err)
+		}
+		//cache the result
+		c.cache.Add(url, data)
+		resp.Body.Close()
 	}
 
+
+	//unmarshal the data and return in the form of an ApiStruct 
 	locationResp := ApiStruct{}
-	if err = json.Unmarshal(data, &locationResp); err != nil {
+	if err := json.Unmarshal(data, &locationResp); err != nil {
 		return ApiStruct{}, fmt.Errorf("unable to unmarshal http response body: %w", err)
 	}
-
 	return locationResp, nil
 }
 
